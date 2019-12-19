5eef58bcc986bd3405996bb1247c77e05167b36c
Amogh Mishra <amoghmishra23@gmail.com>
2019-12-19 04:27:49 -0500

    Added Content based recommendation

---
+++ src/Content-Recommendation/content_recommendation.py
@@ -0,0 +1,74 @@
+import json
+from annoy import AnnoyIndex
+from models import InferSent
+import torch
+
+
+model_version = 2
+MODEL_PATH = "infersent%s.pkl" % model_version
+params_model = {'bsize': 64, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
+                'pool_type': 'max', 'dpout_model': 0.0, 'version': model_version}
+model = InferSent(params_model)
+model.load_state_dict(torch.load(MODEL_PATH))
+
+# use_cuda = False
+# model = model.cuda() if use_cuda else model
+
+# If infersent1 -> use GloVe embeddings. If infersent2 -> use InferSent embeddings.
+W2V_PATH = 'GloVe/glove.840B.300d.txt' if model_version == 1 else '../../model/fastText/crawl-300d-2M.vec'
+model.set_w2v_path(W2V_PATH)
+
+# Load embeddings of K most frequent words
+model.build_vocab_k_words(K=100000)
+
+dim = 4096
+
+def add_annoy_index(path):
+    dim = 4096
+    an = AnnoyIndex(dim, 'angular')
+    print("STARTED ANNOY INDEX.")
+    with open(path,'r',encoding='utf-8') as fr:
+        for i in fr:
+            data = i.split("\t")
+            index = data[0]
+            embedding = eval(data[1].strip())
+            an.add_item(int(index), embedding)
+
+    an.build(10)
+    an.save('../../model/business.ann')
+    print("ANNOY INDEXING FINISHED.")
+
+def get_business_data():
+    business = {}
+    with open('../../data/business.json') as f:
+        for i in f:
+            data = json.loads(i)
+            business[data["business_id"]] = data["name"]
+    return business
+
+def get_num_to_bid(bid_to_json):
+    n_to_b = {}
+    for i,j in bid_to_json.items():
+        n_to_b[j]=i
+    return n_to_b
+
+def load_data(reviews):
+    an = AnnoyIndex(4096, 'angular')
+    an.load('../../model/business.ann')
+    print(an.get_n_items())
+    bid_to_json = json.load(open('../../data/bid.json'))
+    num_to_bid = get_num_to_bid(bid_to_json)
+    business = get_business_data()
+    embeddings = model.encode([reviews], bsize=1, tokenize=False, verbose=True)
+    # ids = an.get_nns_by_item(0, 10, search_k=-1, include_distances=False)
+    ids = an.get_nns_by_vector(embeddings[0], 10, search_k=-1, include_distances=False)
+    result = []
+    for i in ids:
+        result.append(business[num_to_bid[i]])
+    print(result)
+
+if __name__=='__main__':
+    path = '../../output/review_embedding.txt'
+    # add_annoy_index(path)
+    load_data("This was the best pizza place ever")
+    load_data("Worst ambience ever. Never go to this place")
---
+++ src/Content-Recommendation/test.py
@@ -0,0 +1,105 @@
+from random import randint
+import numpy as np
+import torch
+from models import InferSent
+model_version = 2
+MODEL_PATH = "infersent%s.pkl" % model_version
+params_model = {'bsize': 64, 'word_emb_dim': 300, 'enc_lstm_dim': 2048,
+                'pool_type': 'max', 'dpout_model': 0.0, 'version': model_version}
+model = InferSent(params_model)
+model.load_state_dict(torch.load(MODEL_PATH))
+
+use_cuda = True
+model = model.cuda() if use_cuda else model
+
+# If infersent1 -> use GloVe embeddings. If infersent2 -> use InferSent embeddings.
+W2V_PATH = 'GloVe/glove.840B.300d.txt' if model_version == 1 else '../../model/fastText/crawl-300d-2M.vec'
+model.set_w2v_path(W2V_PATH)
+
+# Load embeddings of K most frequent words
+model.build_vocab_k_words(K=100000)
+
+import csv
+
+
+def get_top_business_reviews(business_reviews, review_weight):
+    desc_business_rev = {}
+    desc_rev_weight = sorted(review_weight, key=review_weight.get, reverse=True)
+    for bid in review_weight:
+        indexes = sorted(range(len(review_weight[bid])), key=lambda i: review_weight[bid][i], reverse=True)[:20]
+        desc_business_rev[bid] = [business_reviews[bid][indexes[0]]]
+    return desc_business_rev
+
+def get_agg_business_reviews():
+    print("==Reading business dataset==")
+    reviews_emb, ids, ids_to_num, count = [], [], {}, 0
+    bid_to_num = {}
+    business_reviews = {}
+    review_weight = {}
+    reader = csv.DictReader(open("../../data/2018-review-LasVegas-Restaurants.csv",encoding='utf-8'))
+    for row in reader:
+        bid = row['business_id']
+        if bid not in bid_to_num:
+            bid_to_num[bid] = count
+            count += 1
+
+        if bid not in business_reviews:
+            business_reviews[bid] = [row['text']]
+            review_weight[bid] = [int(row['useful']) + int(row['cool']) + 1]
+        else:
+            business_reviews[bid].append(row['text'])
+            review_weight[bid].append(int(row['useful']) + int(row['cool']) + 1)
+
+    print("==Reading business dataset finished==")
+
+    business_reviews = get_top_business_reviews(business_reviews, review_weight)
+    return business_reviews, review_weight, bid_to_num
+
+# business_reviews, review_weight, bid_to_num = get_agg_business_reviews()
+# embeddings = model.encode(business_reviews, bsize=128, tokenize=False, verbose=True)
+# print('nb sentences encoded : {0}'.format(len(embeddings)))
+
+# for k,v in business_reviews.items():
+#     print(k)
+#     embeddings = model.encode(v, bsize=128, tokenize=False, verbose=True)
+#     print('nb sentences encoded : {0}'.format(len(embeddings)))
+
+
+def get_business_embedding():
+    print("==Preparing business embedding==")
+    business_reviews, review_weight, bid_to_num = get_agg_business_reviews()
+
+    business_embedding = []
+    count = 0
+    print("Total businesses", len(business_reviews))
+    with open("../../output/review_embedding.txt","w") as fw:
+        for bid, reviews in business_reviews.items():
+            print(count)
+            embeddings = model.encode(reviews, bsize=128, tokenize=False, verbose=True)
+            # print('nb sentences encoded : {0}'.format(len(embeddings)))
+            normalized_weighted_embdding = get_weighted_embedding(embeddings,review_weight[bid])
+            fw.write(str(bid_to_num[bid])+"\t"+ str(normalized_weighted_embdding)+"\n")
+            # business_embedding.append((bid_to_num[bid],normalized_weighted_embdding))
+            count += 1
+    print("Total restaurant", count)
+    print("==Reading business embedding finished==")
+    print()
+    return None
+
+
+def get_weighted_embedding(review_embedding, review_weights):
+    weighted_emb = np.zeros(4096, )
+    total = 0
+    for weight, rev_emb in zip(review_weights, review_embedding):
+        weighted_emb += weight * np.array(rev_emb)
+        total += int(weight)
+    weighted_emb = weighted_emb / total
+    return  weighted_emb.tolist()
+
+# def cosine(u, v):
+#     return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
+#
+# print(cosine(model.encode(['mexican is go food'])[0], model.encode(['Tacos is delicious.'])[0]))
+
+if __name__=="__main__":
+    get_business_embedding()
